6e9c51609a13bada3b8c6bf6943a1a81b2e4330c
Obeth Ortiz <obetho@gmail.com>
2020-07-03 13:31:04 -0500

    se agrega Docs

---
+++ docs/precache-manifest.20ca60c2ea53e0fdd53e469ca49f785c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c130e73704e22a20856edbf284559d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3651157c94bae089716",
+    "url": "/static/css/main.8ba5af24.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3651157c94bae089716",
+    "url": "/static/js/main.9b25f60e.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
